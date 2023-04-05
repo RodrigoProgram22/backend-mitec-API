7cd74beb2b77b33dd2ac8a99a97afac667ea997f
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2023-04-05 15:59:10 -0300

    Proyecto Base .2

--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/UsuarioController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/UsuarioController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @CrossOrigin (origins = "https://mi-tec.netlify.app")
 //@CrossOrigin(origins = "http://localhost:4200")
