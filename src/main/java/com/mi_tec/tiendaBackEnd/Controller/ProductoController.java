7e6cec5e9657205a2040d028a01c71d951cc6a43
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2023-08-11 16:26:05 -0300

    Se cambio el CrossOrigin para Hosting.

--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/ProductoController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/ProductoController.java
@@ -22,8 +22,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin (origins = "https://mi-tec.netlify.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://mi-tec.netlify.app")
 public class ProductoController {
     @Autowired
     IProductoService iProducS;
