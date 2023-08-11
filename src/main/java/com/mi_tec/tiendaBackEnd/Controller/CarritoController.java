e26d305553388e655ba049ac2df8e7cb828d523a
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2023-08-11 14:00:01 -0300

    Se cambio el CrossOrigin.

--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/CarritoController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/CarritoController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin (origins = "https://mi-tec.netlify.app")
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin (origins = "https://mi-tec.netlify.app")
 @RequestMapping("/carrito")
 public class CarritoController {
 
--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/UsuarioController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/UsuarioController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "https://mi-tec.netlify.app")
-//@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin (origins = "https://mi-tec.netlify.app")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
    
     @Autowired
