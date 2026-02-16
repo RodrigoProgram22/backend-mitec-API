3c9806603ccc073716c935199ddcbe9113bee500
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2026-02-16 00:39:29 -0300

    Se cambio los crossorigin.

--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/ProductoController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/ProductoController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "https://mitecrodri.online")
+@CrossOrigin (origins = "https://mitecrodri.online/")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
     @Autowired
