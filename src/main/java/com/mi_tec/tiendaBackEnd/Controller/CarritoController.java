a6ca78fc5671243abcab6d5b8f1ccea4e0b1a79a
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2026-02-16 00:48:53 -0300

    Se cambio los crossorigin 2.

--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/CarritoController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/CarritoController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "https://mitecrodri.online/")
+@CrossOrigin (origins = "https://mitecrodri.online")
 //@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/carrito")
 public class CarritoController {
--- src/main/java/com/mi_tec/tiendaBackEnd/Controller/UsuarioController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Controller/UsuarioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "https://mitecrodri.online/")
+@CrossOrigin (origins = "https://mitecrodri.online")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
    
