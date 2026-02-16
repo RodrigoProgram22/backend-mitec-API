80343947d06bf7a4cb878cd6802aa9c4430568ed
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2026-02-15 23:19:11 -0300

    Se cambio la direccion de la BBDD y el dominio.

--- src/main/java/com/mi_tec/tiendaBackEnd/Security/Controller/AuthController.java
+++ src/main/java/com/mi_tec/tiendaBackEnd/Security/Controller/AuthController.java
@@ -34,8 +34,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-//@CrossOrigin (origins = "https://mitec.store/")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://mitecrodri.online")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     @Autowired
